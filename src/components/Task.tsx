7cacc33cdbd2aa9b2951dbebc1703d57d1526c5d
Victor Goecking <victorgoecking96@gmail.com>
2022-08-03 22:31:00 -0300

    :sparkles: finalizing layout

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,9 +4,16 @@ import { Trash } from 'phosphor-react';
 export function Task() {
   return (
     <div className={styles.taskBox}>
-      <div className={styles.taskContent}>
+      {/* <div className={styles.taskContent}>
         <input type="checkbox" />
         <span>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer. Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer. Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer. Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
+      </div> */}
+      <div className={styles.taskContent}>
+        <label className={styles.checkboxContainer}>
+          <input type="checkbox" />
+          <span className={styles.checkmark}></span>
+        </label>
+        <span className={styles.descriptionTask}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
       </div>
       <button title="Deletar tarefa">
         <Trash size={14} />
